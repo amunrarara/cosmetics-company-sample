04daaf0240a49433ddaa5019439d803b1cd13dbc
Acea Spades <aceaspades.black@gmail.com>
2022-12-04 01:08:46 +0200

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function TemplateHomepage({}: Props) {
+  return (
+    <div>TemplateHomepage</div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-disable import/no-extraneous-dependencies */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  important: true,
+  mode: "jit",
+  content: [
+    "./src/styles/**/*.css",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      mobilenav: { max: '1100px' },
+      desktopnav: { min: '1101px' },
+      ...defaultTheme.screens,
+    },
+    extend: {
+      fontSize: {
+        'navbar-height': 'var(--navbar-height)',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s ease-out infinite',
+      },
+      dropShadow: {
+        'dark': '0 3px 3px rgba(0, 0, 0, 0.75)',
+        'darkest': '0 2px 2px rgba(0, 0, 0, 1)',
+      },
+    },
+  },
+  plugins: [],
+};
